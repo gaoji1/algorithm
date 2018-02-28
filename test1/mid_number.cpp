6bfbbdc53a1a7f0df2fa1226fad208dde3ca6194
example name <974632489@qq.com>
2018-02-28 10:10:56 +0800

    new

---
+++ test1/mid_number.cpp
@@ -0,0 +1,18 @@
+//ÖÐÎ»Êý
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        int input[n];
+        for(int i=0;i<n;i++)
+        {
+            int temp;
+            cin>>temp;
+            input[i] = temp;
+        }
+    }
+}
